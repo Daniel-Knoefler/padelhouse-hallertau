a590af7236b7390f354e8e24d270a29593d31d8d
Daniel-Knoefler <daniel.knoefler@outlook.de>
2026-08-20 21:09:23 +0200

    Enhance main.jsx to clear app badge on render
    Added a check to clear app badge if supported by the navigator.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from "react"; 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactDOM from "react-dom/client"; 
+
+import App from "./App.jsx"; 
+
+import "./index.css"; 
+
+  
+
+if (typeof navigator !== "undefined" && "clearAppBadge" in navigator) { 
+
+  navigator.clearAppBadge().catch(() => {}); 
+
+} 
+
+  
+
+ReactDOM.createRoot(document.getElementById("root")).render( 
+
+  <React.StrictMode> 
+
+    <App /> 
+
+  </React.StrictMode> 
+
+); 
+
+  
